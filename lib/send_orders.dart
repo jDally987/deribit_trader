c008ac13e6a79878a5e9557626dea28a2651eba1
John Chaney <jdally987@gmail.com>
2019-08-31 21:44:18 -0400

    Got (testnet) API keys read in. Not committing the APIKEYS.dart file obviously.

--- lib/send_orders.dart
+++ lib/send_orders.dart
@@ -2,19 +2,40 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import 'APIKEYS.dart';
+
 import 'package:http/http.dart' as http;
 
 //import 'package:flutter_flux/flutter_flux.dart';
 
 
-class DeribitClient with SecureStoreMixin {
-//  final String api_key;
-//  final String api_secret;
-//  final bool testnet;
+class DeribitClient {
+  bool testnet;
 
-  final String baseUrlTest = 'https://test.deribit.com/api/v2/';
+  final String baseUrl = 'https://www.deribit.com/api/v2/';
+  final String baseUrlTestnet = 'https://test.deribit.com/api/v2/';
 //  DeribitClient({this.api_key, this.api_secret, this.testnet})
 
+
+  DeribitClient () {
+    final secureStore = FlutterSecureStorage();
+
+    secureStore.deleteAll();
+
+    secureStore.read(key: 'apiKey').then( (String val) {
+
+      print("Initially, the value of apiKey is: $val");
+
+      if (val == null) {
+        // pull from text file
+         secureStore.write(key: 'apiKey', value: API_KEY);
+         secureStore.write(key: 'apiSecret', value: API_SECRET);
+      }
+      secureStore.read(key: 'apiKey').then( (String newVal) => print("Ok, final value in apiKey is: $newVal"));
+    });
+
+    testnet = true;
+  }
 
 
 
@@ -22,7 +43,7 @@ class DeribitClient with SecureStoreMixin {
 //    var client = http.Client();
 //    var streamedresp = await client.send(http.Request('get', Uri.parse(this.baseUrlTest + 'public/get_index' + '?currency=BTC')));
 
-    http.Response getresp = await http.get(this.baseUrlTest + 'public/get_index' + '?currency=BTC');
+    http.Response getresp = await http.get(this.baseUrlTestnet + 'public/get_index' + '?currency=BTC');
 
     if (getresp.statusCode == 200){
       return json.decode(getresp.body)['result']['BTC'].toString();   // return result
@@ -49,6 +70,14 @@ class SecureStoreMixin {
 
   void getSecureStore(String key, Function callback) async {
     await secureStore.read(key: key).then(callback);
+  }
+
+  Future<Map<String, String>> allValues(Function callback) {
+    return secureStore.readAll().then(callback);
+  }
+
+  void deleteAllSecureStorage() async {
+    await secureStore.deleteAll();
   }
 
   void printLol() {
